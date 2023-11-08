c0539a14439c5733b967dbad3e334d96a5429e72
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-11-08 18:57:23 -0400

    GastoDto Añadido.

---
+++ app/src/main/java/com/example/emill_p2_ap2/data/remote/dto/GastoDto.kt
@@ -0,0 +1,14 @@
+package com.example.emill_p2_ap2.data.remote.dto
+import androidx.room.PrimaryKey
+
+data class GastoDto (
+    @PrimaryKey
+    var idGasto: Int? = null,
+    var fecha: String = "",
+    var idSuplidor: Int? = null,
+    var suplidor: String = "",
+    var concepto: String = "",
+    var ncf: String = "",
+    var itbis: Double = 0.0,
+    var monto: Double = 0.0
+)
